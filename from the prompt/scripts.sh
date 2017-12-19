ab360c772beda363bb2485f87f43ae8f41d01282
Carlo Costanzo <Carlo@ipm.com>
2017-12-19 01:29:15 -0500

    Looks pretty Good now! :)

--- from the prompt/scripts.sh
+++ from the prompt/scripts.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 # this script uses hard-coded paths 
+# This deletes the readme file and begins rebuilding it.. 
+rm readme.md
+echo "# Original work by @beed2112" >> readme.md
+echo "# Modified by @CCOSTAN" >> readme.md
+echo "# Repo : https://github.com/CCOSTAN/Home-AssistantConfig" >> readme.md
+echo "# Repo : https://github.com/beed2112/condo " >> readme.md
+
 grep -e '^[a-z]' /home/hass/.homeassistant/script/*.yaml | tr : '\n' > /tmp/scriptnames.txt
 
 
@@ -10,7 +17,10 @@ do
 # for now just repeating the the "res=" statement to add directories  
 
   res=`grep -R script.$item /home/hass/.homeassistant/. --include=*.yaml`
-  echo "$item "
-  echo "$res  "
+  echo "**-------------------------------------------------------------------------**" >> readme.md
+  echo "$item " >> readme.md
+  echo "**-------------------------------------------------------------------------**" >> readme.md
+  echo "$res  " >> readme.md
 
 done
+cat readme.md
