e07520bdad5eeacdab8a54b29117e62d0270e338
Carlo Costanzo <Carlo@ipm.com>
2017-12-19 01:39:53 -0500

    Update scripts.sh

--- from the prompt/scripts.sh
+++ from the prompt/scripts.sh
@@ -2,11 +2,12 @@
 
 # this script uses hard-coded paths 
 # This deletes the readme file and begins rebuilding it.. 
-rm readme.md
-echo "# Original work by @beed2112" >> readme.md
-echo "# Modified by @CCOSTAN" >> readme.md
-echo "# Repo : https://github.com/CCOSTAN/Home-AssistantConfig" >> readme.md
-echo "# Repo : https://github.com/beed2112/condo " >> readme.md
+rm /home/hass/.homeassistant/script/readme.md
+echo "# Original work by @beed2112" >> /home/hass/.homeassistant/script/readme.md
+echo "# Modified by @CCOSTAN" >> /home/hass/.homeassistant/script/readme.md
+echo "# Repo : https://github.com/CCOSTAN/Home-AssistantConfig" >> /home/hass/.homeassistant/script/readme.md
+echo "# Repo : https://github.com/beed2112/condo " >> /home/hass/.homeassistant/script/readme.md
+echo "# This is a map of the script references from my repo. " >> /home/hass/.homeassistant/script/readme.md
 
 grep -e '^[a-z]' /home/hass/.homeassistant/script/*.yaml | tr : '\n' > /tmp/scriptnames.txt
 
@@ -17,10 +18,10 @@ do
 # for now just repeating the the "res=" statement to add directories  
 
   res=`grep -R script.$item /home/hass/.homeassistant/. --include=*.yaml`
-  echo "**-------------------------------------------------------------------------**" >> readme.md
-  echo "$item " >> readme.md
-  echo "**-------------------------------------------------------------------------**" >> readme.md
-  echo "$res  " >> readme.md
+  echo "**-------------------------------------------------------------------------**" >> /home/hass/.homeassistant/script/readme.md
+  echo "$item " >> /home/hass/.homeassistant/script/readme.md
+  echo "**-------------------------------------------------------------------------**" >> /home/hass/.homeassistant/script/readme.md
+  echo "$res  " >> /home/hass/.homeassistant/script/readme.md
 
 done
-cat readme.md
+cat /home/hass/.homeassistant/script/readme.md
