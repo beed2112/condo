1ace18881df26629708d08d6a2b595516be92996
Carlo Costanzo <Carlo@ipm.com>
2017-12-19 01:13:34 -0500

    This seems to work really well now.

--- from the prompt/scripts.sh
+++ from the prompt/scripts.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-# this script uses hardcoded pathes 
+# this script uses hard-coded paths 
 grep -e '^[a-z]' /home/hass/.homeassistant/script/*.yaml | tr : '\n' > /tmp/scriptnames.txt
 
 
 for item in `cat /tmp/scriptnames.txt`
 do
 #recursion does not seem to work 
-# for now just repeating the the "res=" statement to add directoryies  
+# for now just repeating the the "res=" statement to add directories  
 
-  res=`grep -R script.$item /home/homeassistant/.homeassistant/*.yaml`
-  res2=`grep -R script.$item /home/homeassistant/.homeassistant/automations/*.yaml`
+  res=`grep -R script.$item /home/hass/.homeassistant/. --include=*.yaml`
   echo "$item "
   echo "$res  "
-  echo "$res2  "
+
 done
